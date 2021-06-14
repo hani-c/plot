a5483071fa315a96113a0a5ebc23233d57abf860
hani-c <73563227+hani-c@users.noreply.github.com>
2021-06-14 13:31:18 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,24 +2,20 @@
 // Data is joined to map using value of 'hc-key' property by default.
 // See API docs for 'joinBy' for more info on linking data and map.
 var data = [
-    ['fr-bre', 0],
-    ['fr-pdl', 1],
-    ['fr-pac', 2],
-    ['fr-occ', 3],
-    ['fr-naq', 4],
-    ['fr-bfc', 5],
-    ['fr-cvl', 6],
-    ['fr-idf', 7],
-    ['fr-hdf', 8],
-    ['fr-ara', 9],
-    ['fr-ges', 10],
-    ['fr-nor', 11]
+    ['fr-idf-se', 0],
+    ['fr-idf-hd', 1],
+    ['fr-idf-ss', 2],
+    ['fr-idf-es', 3],
+    ['fr-idf-vo', 4],
+    ['fr-idf-vp', 5],
+    ['fr-idf-vm', 6],
+    ['fr-idf-yv', 7]
 ];
 
 // Create the chart
 Highcharts.mapChart('container', {
     chart: {
-        map: 'countries/fr/custom/fr-all-mainland'
+        map: 'countries/fr/fr-idf-all'
     },
 
     title: {
@@ -27,7 +23,7 @@ Highcharts.mapChart('container', {
     },
 
     subtitle: {
-        text: 'Source map: <a href="http://code.highcharts.com/mapdata/countries/fr/custom/fr-all-mainland.js">France, mainland</a>'
+        text: 'Source map: <a href="http://code.highcharts.com/mapdata/countries/fr/fr-idf-all.js">Île-de-France</a>'
     },
 
     mapNavigation: {
