61c359795b2cd8d5e80511cb94be170e769b1ddf
hani-c <73563227+hani-c@users.noreply.github.com>
2021-06-14 13:02:52 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,25 +1,116 @@
-document.addEventListener('DOMContentLoaded', function () {
-        const chart = Highcharts.chart('container', {
-            chart: {
-                type: 'bar'
+Highcharts.getJSON('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/australia.geo.json', function (geojson) {
+
+    // Prepare the geojson
+    var states = Highcharts.geojson(geojson, 'map'),
+        rivers = Highcharts.geojson(geojson, 'mapline'),
+        cities = Highcharts.geojson(geojson, 'mappoint'),
+        specialCityLabels = {
+            Melbourne: {
+                align: 'right'
             },
-            title: {
-                text: 'Fruit Consumption'
+            Canberra: {
+                align: 'right',
+                y: -5
             },
-            xAxis: {
-                categories: ['Apples', 'Bananas', 'Oranges']
+            Wollongong: {
+                y: 5
             },
-            yAxis: {
-                title: {
-                    text: 'Fruit eaten'
+            Brisbane: {
+                y: -5
+            }
+        };
+
+    // Skip or move some labels to avoid collision
+    states.forEach(function (state) {
+        // Disable data labels
+        if (state.properties.code_hasc === 'AU.CT' || state.properties.code_hasc === 'AU.JB') {
+            state.dataLabels = {
+                enabled: false
+            };
+        }
+        if (state.properties.code_hasc === 'AU.TS') {
+            state.dataLabels = {
+                style: {
+                    color: '#333333'
+                }
+            };
+        }
+        // Move center for data label
+        if (state.properties.code_hasc === 'AU.SA') {
+            state.middleY = 0.3;
+        }
+        if (state.properties.code_hasc === 'AU.QL') {
+            state.middleY = 0.7;
+        }
+    });
+
+    cities.forEach(function (city) {
+        if (specialCityLabels[city.name]) {
+            city.dataLabels = specialCityLabels[city.name];
+        }
+    });
+
+    // Initiate the chart
+    Highcharts.mapChart('container', {
+        title: {
+            text: 'Highmaps from geojson with multiple geometry types'
+        },
+
+        mapNavigation: {
+            enabled: true,
+            buttonOptions: {
+                verticalAlign: 'bottom'
+            }
+        },
+
+        series: [{
+            name: 'States and territories',
+            data: states,
+            color: Highcharts.getOptions().colors[2],
+            states: {
+                hover: {
+                    color: Highcharts.getOptions().colors[4]
                 }
             },
-            series: [{
-                name: 'Jane',
-                data: [1, 0, 4]
-            }, {
-                name: 'John',
-                data: [5, 7, 3]
-            }]
-        });
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}',
+                style: {
+                    width: '80px' // force line-wrap
+                }
+            },
+            tooltip: {
+                pointFormat: '{point.name}'
+            }
+        }, {
+            name: 'Rivers',
+            type: 'mapline',
+            data: rivers,
+            states: {
+                hover: {
+                    lineWidth: 3
+                }
+            },
+            color: Highcharts.getOptions().colors[0],
+            tooltip: {
+                pointFormat: '{point.properties.NAME}'
+            }
+        }, {
+            name: 'Cities',
+            type: 'mappoint',
+            data: cities,
+            color: 'black',
+            marker: {
+                radius: 2
+            },
+            dataLabels: {
+                align: 'left',
+                verticalAlign: 'middle'
+            },
+            animation: false,
+            tooltip: {
+                pointFormat: '{point.name}'
+            }
+        }]
     });
+});
