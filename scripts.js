4441ecaff7b65e9afa0a1b40ec1a9545b3b6510e
hani-c <73563227+hani-c@users.noreply.github.com>
2021-06-23 15:23:53 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,37 @@
 // Prepare demo data
 // Data is joined to map using value of 'hc-key' property by default.
 // See API docs for 'joinBy' for more info on linking data and map.
-var data = [
-    ['fr-bre', 0],
-    ['fr-pdl', 1],
-    ['fr-pac', 2],
-    ['fr-occ', 3],
-    ['fr-naq', 4],
-    ['fr-bfc', 5],
-    ['fr-cvl', 6],
-    ['fr-idf', 7],
-    ['fr-hdf', 8],
-    ['fr-ara', 9],
-    ['fr-ges', 10],
-    ['fr-nor', 11]
-];
+var chart, dataSequence = [
+     {
+            name: '2009',
+            data: [1, 2, 2, 1, 1, 2, 2, 2, 1, 1,5]
+        }, {
+            name: '2010',
+            data: [6, 12, 2, 3, 3, 2, 2, 3, 2, 2,5]
+        }, {
+            name: '2011',
+            data: [4, 5, 6, 5, 5, 4, 9, 5, 3, 4,5]
+        }, {
+            name: '2012',
+            data: [5, 5, 6, 6, 5, 6, 6, 5, 5, 6, 5]
+        }, {
+            name: '2013',
+            data: [6, 7, 7, 6, 6, 6, 7, 7, 6, 7, 5]
+        }, {
+            name: '2014',
+            data: [8, 9, 9, 8, 8, 8, 9, 9, 8, 9, 5]
+        }, {
+            name: '2015',
+            data: [9, 10, 4, 10, 9, 9, 9, 10, 10, 10, 5]
+        }, {
+            name: '2016',
+            data: [1, 10, 10, 10, 10, 11, 11, 11, 12, 12, 5]
+        }, {
+            name: '2017',
+            data: [11, 11, 12, 12, 12, 11, 11, 12, 12, 12, 5]
+        }
+    ];
+    
 
 // Create the chart
 Highcharts.mapChart('container', {
@@ -23,7 +40,7 @@ Highcharts.mapChart('container', {
     },
 
     title: {
-        text: 'Highmaps basic demo'
+        text: 'Highmaps with time control'
     },
 
     subtitle: {
@@ -38,11 +55,14 @@ Highcharts.mapChart('container', {
     },
 
     colorAxis: {
-        min: 0
+        min: 0,
+        max: 12
     },
 
     series: [{
-        data: data,
+        data: dataSequence[0].data.slice(),
+        mapData: Highcharts.maps['countries/fr/custom/fr-all-mainland'],
+        joinBy: null,
         name: 'Random data',
         states: {
             hover: {
@@ -56,7 +76,65 @@ Highcharts.mapChart('container', {
     }]
 });
 
+ /**
+     * Update the chart. This happens either on updating (moving) the range input,
+     * or from a timer when the timeline is playing.
+     */
+    function update(increment) {
+        var input = $('#play-range')[0],
+            output = $('#play-output')[0];
 
+        if (increment) {
+            input.value = parseInt(input.value) + increment;
+        }
+        chart.series[0].setData(dataSequence[input.value].data); // Increment dataset (updates chart)
+        output.innerHTML = dataSequence[input.value].name; // Output value
+        if (input.value >= input.max) { // Auto-pause
+            pause($('#play-pause-button')[0]);
+        }
+    }
+
+    /**
+     * Play the timeline.
+     */
+    function play(button) {
+        button.title = 'pause';
+        button.className = 'fa fa-pause';
+        chart.sequenceTimer = setInterval ( function () {
+            update(1);
+        }, 500);
+
+    }
+    
+    /** 
+     * Pause the timeline, either when the range is ended, or when clicking the pause button.
+     * Pausing stops the timer and resets the button to play mode.
+     */
+    function pause(button) {
+        button.title = 'play';
+        button.className = 'fa fa-play';
+        clearTimeout(chart.sequenceTimer);
+        chart.sequenceTimer = undefined;
+    }
+    
+    /**
+     * Toggle play and pause from the button
+     */
+    $('#play-pause-button').bind('click', function () {
+        if (chart.sequenceTimer === undefined) {
+            play(this);
+        } else {
+            pause(this);
+        }
+    });
+    
+    /**
+     * Update the chart when the input is changed
+     */
+    $('#play-range').bind('input', function () {
+        update();
+    });
+});
 
 var data2 = [
     ['fr-idf-se', 0],
